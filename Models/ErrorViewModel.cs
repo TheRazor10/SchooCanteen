bc2069fc0a1bd9959bab7c6108cf4c4f8f5de077
TheRazor10 <georgikrasi1011@gmail.com>
2024-12-18 13:20:37 +0200

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolCanteen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
